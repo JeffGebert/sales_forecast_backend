a732532b9d56da64283fb8acdf0484e5262256e0
Jeff Gebert <jeffgebert@hotmail.com>
2020-02-09 19:25:37 -0500

    made index route ready for db query

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+const Pool = require('pg').Pool
+const app = express();
+const port = 8001
+var cors = require('cors')
+var cookieParser = require('cookie-parser')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+
+const db = new Pool()
+
+var indexRouter = require('./routes/index');
+
+
+
+app.use('/', indexRouter(db));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,4 +48,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
+
+
+app.listen(port, () => {    
+  console.log(`App running on port ${port}.`)
+})
+
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+module.exports = (db) => {
+  
+  router.get('/', function(req, res, next) {
+    res.send("hello jeff you are awesome")
+  });
+  
+  return router;
+
+}
+
